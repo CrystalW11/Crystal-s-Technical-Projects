da41f7edd5159a2d68f1e34196eb5b64343cd9d8
Crystal Warmack <156721017+CrystalW11@users.noreply.github.com>
2024-04-23 11:03:44 -0600

    Delete recipes_belt_prep directory

--- recipes_belt_prep/server.py
+++
@@ -1,9 +0,0 @@
-from flask_app import app
-
-
-# remember to import controllers
-from flask_app.controllers import users
-from flask_app.controllers import recipes
-
-if __name__ == "__main__":
-    app.run(debug=True)
